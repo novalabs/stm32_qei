1a6e7b1cb65320a641bc8e89fa9e8a744b6259a0
Davide Rizzi <d.rizzi@novalabs.io>
2018-01-02 13:09:41 +0100

    Updated copyright

--- include/qei_lld.h
+++ include/qei_lld.h
@@ -1,4 +1,4 @@
-/* COPYRIGHT (c) 2016-2017 Nova Labs SRL
+/* COPYRIGHT (c) 2016-2018 Nova Labs SRL
  *
  * All rights reserved. All use of this software and documentation is
  * subject to the License Agreement located in the file LICENSE.
